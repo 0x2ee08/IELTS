0a253152371331db39f180439f29c6b3fb5d2d48
anHiep <HIE2Pbrizzily@gmail.com>
2024-08-28 07:20:52 +0700

    register worked

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -12,6 +12,11 @@ interface MyComponentState {
 
 const SpeakingPage: React.FC = () => {
     const [ test ] = useState('a');
+    const [ username ] = useState('hsgs');
+    const [ email ] = useState('hsgshackathon@gmail.com');
+    const [ name ] = useState('hsgs2024');
+    const [ password ] = useState('Hsgs2024');
+    const [ role ] = useState('admin');
 
     const saveToDataBase = async () => {
         const token = localStorage.getItem('token');
@@ -20,6 +25,18 @@ const SpeakingPage: React.FC = () => {
                 headers: {
                     'authorization': `Bearer ${token}`
                 }
+            });
+        } finally {
+        }
+    };
+
+    const register = async () => {
+        // const token = localStorage.getItem('token');
+        try {
+            const response = await axios.post(`${config.API_BASE_URL}api/register`, { username, email, name, password, role },{
+                // headers: {
+                //     'authorization': `Bearer ${token}`
+                // }
             });
         } finally {
         }
@@ -34,6 +51,12 @@ const SpeakingPage: React.FC = () => {
                 Up to database
             </button>
 
+            <button
+                className="button p-2 my-4 mb-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+                onClick={register}
+            >
+                Register
+            </button>
         </div>
     );
 };
