b5e33bc05e1fca481f5d7c2ca14f37236fd64860
nhtd <95995023+Khoaham@users.noreply.github.com>
2024-08-28 13:20:21 +0700

    add something useless

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
+// app/page.tsx
+'use client'
+
+import Head from 'next/head';
+import Link from 'next/link';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import Header from './components/Header';
+
+
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Nothing here yet, start at /login and /register</h1>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-200 via-white to-white dark:from-zinc-800 dark:via-black dark:to-black">
+            <Head>
+                <title>Virtual Staging AI</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+
+            Header
+            <Header />
+
+            Main content
+            <MainContent />
+
+            Footer
+            <Footer />
+        </div>
+    );
 }
