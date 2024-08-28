eb16bb6a63a07447b317ad499e84504604ec5067
anHiep <HIE2Pbrizzily@gmail.com>
2024-08-28 14:54:40 +0700

    stash

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -5,6 +5,7 @@ import config from '../config';
 import axios from 'axios';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { useRouter } from 'next/navigation';
 
 const RegisterPage: React.FC = () => {
     const [status, setStatus] = useState('');
@@ -17,6 +18,7 @@ const RegisterPage: React.FC = () => {
     const [tokens, setToken] = useState(10);
     const [role] = useState('student');
     const [schoollist, setSchoollist] = useState<any[]>([]);
+    const router = useRouter();
 
     const register = async () => {
         try {
@@ -33,9 +35,9 @@ const RegisterPage: React.FC = () => {
                 localStorage.setItem('token', result.accessToken);
                 localStorage.setItem('username', result.username);
                 localStorage.setItem('role', result.role);
-    
+
                 // Redirect to user page
-                // router.push('/user');
+                // router.push('/profile');
             } else {
                 alert('Register failed: ' + result.error);
             }
