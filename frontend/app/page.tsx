b6b692d5337f6509e663076343f493a45eaad076
Phong Nguyễn Hoàng <yuki@Phongs-MacBook-Air.local>
2024-08-29 15:23:22 +0700

    homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,6 +12,7 @@ import Header from './components/Header';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import './globals.css'; // 
 
 export default function Home() {
   var Slidersettings = {
@@ -33,9 +34,11 @@ export default function Home() {
 
           <div style={{ width: '100%' }}>
             <Slider {...Slidersettings}>
-              <div>
-                <img src="banner.jpg" alt="Banner" style={{ width: '100%', height: 'auto' }} />
-              </div>
+                <div className="image-container">
+                  <img src="https://s3-alpha-sig.figma.com/img/2744/8c9f/1306bcd9c4f0ba993bad319f136b4704?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ieA1DwqocUkxjA4h-9IudKnCqe1VDCuhVWMQRrMW2AQWMoi6E8lA2Yp0ddHsY2AY6rwNRo88Q0b8ckoZoCFvuVIGRw3nxDLw97yGTCSugbxbRSWwgK9o-LTvCQ9f1F91GtOXLvacfvAhMK7syf51YsWchjH5o2K23kAq9L8pKtFMt9ibJ2FBKgHteEAWRdLY1vqhN83PY4rudCBwcilkZrSYL73~Ku1OEDUnLDZ3Ybe6QoqWD5RFEtfv72~uB~ZIN4GWj8tPCR3Hk6VQd05508DbxADIOrzs1kIfzG-F~URcNHjPKuWIGI0crDGivyFOIvutcZYDQKjS45FhqgffTQ__" alt="Loading" className="cropped-image" />
+                  <div className = "overlay"></div>
+                  {/* <img src="https://s3-alpha-sig.figma.com/img/01ce/79c8/a6d9ead7e7a159eba7d17231e5323dae?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OTMAlDXOWuKaDISfvy6co~Rs1lKAXdYhv8BHXd1xfuX6iwNyDjNAWkbYBNW589uuIvDtXNtIHAkx0TgyANGfen9vSA8y2rk1glZMNlDXON7fvVhvNmoSh8yftGRfLz-EYoGHwfI39mu5cqR2cvFsSsXw5mpoPzEhDaar7S0rBdKZvNjVVXccuYZwzGiGFek~ikaKyWisXEMeRibteNc0HaxOjbDS684X0dG2op4e5UIXeTzt8reWQT0JS-EsaN049YEj5~DFlloIlo0pN-~~3BDqqOaoKpGQnRjsITm3Dfdd8fokH5sNXHCBn94lmFAnVjBk9fIydmdrV98s0Dp2fw__" alt="Loading" className="overlay-image" /> */}
+                </div>
             </Slider>
           </div>
           <br />
