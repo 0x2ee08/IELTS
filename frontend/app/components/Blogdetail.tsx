f1281e924254d0a0563e26f8a01d1583db99d891
anHiep <HIE2Pbrizzily@gmail.com>
2024-08-30 23:24:11 +0700

    tiny bug (change from str to int done)

--- frontend/app/components/Blogdetail.tsx
+++ frontend/app/components/Blogdetail.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import styles from './ContestDetails.module.css';
 import config from '../config';
 import { useSearchParams } from "next/navigation";
 
@@ -10,6 +8,10 @@ const Blogdetail: React.FC = () => {
     const [ title, setTitle ] = useState('');
     const [ content, setContent ] = useState('');
     const [ author, setAuthor ] = useState('');
+    const [ curlike, setCurlike ] = useState(0);
+    const [ curdislike, setCurdislike ] = useState(0);
+    const [ liked, setLiked ] = useState(false);
+    const [ disliked, setDisliked ] = useState(false);
 
     const get_blog = async () => {
         const token = localStorage.getItem('token');
@@ -25,12 +27,18 @@ const Blogdetail: React.FC = () => {
             setTitle(result.title);
             setContent(result.content);
             setAuthor(result.author);
+            setCurlike(Number(result.like));
+            setCurdislike(Number(result.dislike));
 
         } catch (error) {
             console.error('Error fetching blog:', error);
             alert('internal server error');
         }
     };
+
+    const handleLike = async () => {
+
+    }
 
     useEffect(() => {
         get_blog();
@@ -41,6 +49,11 @@ const Blogdetail: React.FC = () => {
             <p><strong>Title:</strong> {title}</p>
             <p><strong>Author:</strong> {author}</p>
             <p><strong>Content:</strong> {content}</p>
+            <button 
+                onClick={handleLike}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Like {curlike}
+            </button>
         </div>
     );
 };
