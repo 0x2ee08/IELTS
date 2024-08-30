c78a06e8b05ac4577ed6b9c9c0d8d25c3c0b7042
0x2ee08 <1923012716718@thcsnamtuliem.edu.vn>
2024-08-30 21:18:23 +0700

    fix tedtalk

--- frontend/app/tedtalk/page.tsx
+++ frontend/app/tedtalk/page.tsx
@@ -22,11 +22,11 @@ const ManagementPage: React.FC = () => {
                 <div className="w-full flex flex-row">
                     <div className="w-full flex flex-row">
                         {/* Video */}
-                        <div className="w-1/2 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md ml-4">
+                        <div className="w-3/5 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md ml-4">
                         </div>
 
                         {/* Script */}
-                        <div className="w-1/2 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md mr-4">
+                        <div className="w-2/5 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md mr-4">
                         </div>
                     </div>
                 </div>
@@ -35,11 +35,11 @@ const ManagementPage: React.FC = () => {
                 <div className="w-full flex flex-row">
                     <div className="w-full flex flex-row">
                         {/* Note */}
-                        <div className="w-1/2 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md ml-4">
+                        <div className="w-3/5 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md ml-4">
                         </div>
 
                         {/* Chatbot*/}
-                        <div className="w-1/2 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md mr-4">
+                        <div className="w-2/5 container mx-2 my-4 p-4 border border-gray-300 rounded shadow-md mr-4">
                         </div>
                     </div>
                 </div>
