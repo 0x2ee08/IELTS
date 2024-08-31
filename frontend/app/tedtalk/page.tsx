d7d23dedb5a43c812e179d47eb2a32f09a62abc9
nhtd <nguyenhoangtiendata3@gmail.com>
2024-08-31 11:05:19 +0700

    designed tedtalk p2

--- frontend/app/tedtalk/page.tsx
+++ frontend/app/tedtalk/page.tsx
@@ -62,30 +62,54 @@ const MyPage: React.FC = () => {
                     </div>
 
                     {/* Personal Notes */}
-                    <div style={{ padding: '10px', backgroundColor: '#f1f1f1', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', position: 'relative' }}>
-                        <h3 style={{ fontSize: '25px' , fontWeight: 'bold' , margin: '0 0 10px 0', color: '#009bdb' }}>Personal note:</h3>
-                        <textarea
-                            value={notes}
-                            onChange={handleNoteChange}
-                            style={{
-                                width: '100%',
-                                height: '100px',
-                                border: 'none',
-                                outline: 'none',
-                                padding: '10px',
-                                borderRadius: '10px',
-                                boxSizing: 'border-box',
-                                backgroundColor: '#fff',
-                                fontFamily: 'Arial, sans-serif',
-                                fontSize: '14px',
-                                lineHeight: '1.5',
-                                color: '#333',
-                                resize: 'none',
-                                backgroundImage: 'url(/path_to_grid_background.png)',
-                                backgroundSize: 'contain',
-                            }}
-                            placeholder="Write your notes here..."
+                    <div style={{ 
+                        padding: '10px', 
+                        position: 'relative', 
+                        borderRadius: '10px', 
+                        boxShadow: '0 0px 0px rgba(0, 0, 0, 0.1)' 
+                    }}>
+                        <div style={{
+                            backgroundImage: 'url(https://s3-alpha-sig.figma.com/img/6fcf/2061/ae4b0cd5da8149b7f430a7ad4198c1b0?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KlSDz23WBPGx-swvJTt6vSePBWBqPKZNh1XqvY61GynewD02VQ~~cNFXSmWsZmJH9Qq3P97PObKGr4louRFZPxUl2v4M~5TSgXrluDvZXaXYBzG3NiEi9MFNDlJteRirsvjTMufBgPLxfYmLUJJ4aGPQ-woEdOxNNYQdJSpftDB-Y4WFAZWyDqofTeFqXd-puaomrTMV-5bENz4yXrCWfe2HslcPuXmZXxHClt5weuijeFSsppeS6Mmaiq2GOun8JIoNQcxmYI~~y15DbRXBYnMlA45fhIBmmoH4woNUt5O7mTxmqrU9Jmhqu2C~~mBOBQElahpP38RYxJqDH2mctA__)',
+                            backgroundSize: 'contain', 
+                            borderRadius: '10px', 
+                            opacity: 0.2, 
+                            position: 'absolute', 
+                            top: 0, 
+                            left: 0, 
+                            right: 0, 
+                            bottom: 0, 
+                            zIndex: 1 
+                        }}></div>
+                        <div style={{ 
+                            position: 'relative',
+                            zIndex: 2 
+                        }}>
+                            <h3 style={{ 
+                                fontSize: '25px', 
+                                fontWeight: 'bold',
+                                color: '#00B4D8' 
+                            }}>Personal note:</h3>
+                            <textarea
+                                value={notes}
+                                onChange={handleNoteChange}
+                                style={{
+                                    width: '100%',
+                                    height: '110px',
+                                    border: 'none',
+                                    outline: 'none',
+                                    padding: '10px',
+                                    borderRadius: '10px',
+                                    boxSizing: 'border-box',
+                                    backgroundColor: 'transparent',
+                                    fontFamily: 'Arial, sans-serif',
+                                    fontSize: '14px',
+                                    lineHeight: '1.5',
+                                    color: '#333',
+                                    resize: 'none',
+                                }}
+                                placeholder="Write your notes here..."
                             ></textarea>
+                        </div>
                     </div>
                 </div>
 
@@ -105,6 +129,7 @@ const MyPage: React.FC = () => {
                             fontSize: '24px',
                             margin: '0',
                         }}>TRANSCRIPT</h1>
+                        <div className="border-b border-blue-500 mt-2"></div>
                         <div style={{
                             color: '#555',
                             fontSize: '14px',
@@ -130,31 +155,33 @@ const MyPage: React.FC = () => {
                     {/* Chat Bot */}
                     <div style={{ padding: '10px', backgroundColor: '#0077B6', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column' }}>
                         <h3 style={{ fontSize: '20px' , fontWeight: 'bold' , margin: '0 0 10px 0', color: '#FFFFFF' }}>AI chat bot BETA</h3>
-                        <div style={{ flex: 1, overflowY: 'auto', marginBottom: '10px', paddingRight: '10px' }}>
-                            {messages.map((message, index) => (
-                                <div key={index} style={{ textAlign: message.user ? 'right' : 'left', marginBottom: '10px' }}>
-                                    <div
-                                        style={{
-                                            display: 'inline-block',
-                                            padding: '8px',
-                                            borderRadius: '10px',
-                                            background: message.user ? '#e0f7fa' : '#e0e0e0',
-                                            maxWidth: '70%',
-                                            wordWrap: 'break-word',
-                                            color: '#333',
-                                        }}
-                                        >
-                                        {message.text}
+                        <div style={{ padding: '10px', backgroundColor: '#FFFFFF', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column' }}>
+                            <div style={{ flex: 1, overflowY: 'auto' as const, marginBottom: '10px', paddingRight: '10px', maxHeight: '100px', }}>
+                                {messages.map((message, index) => (
+                                    <div key={index} style={{ textAlign: message.user ? 'right' : 'left', marginBottom: '10px' }}>
+                                        <div
+                                            style={{
+                                                display: 'inline-block',
+                                                padding: '8px',
+                                                borderRadius: '10px',
+                                                background: message.user ? '#E5E5E5' : '#00B4D8',
+                                                maxWidth: '70%',
+                                                wordWrap: 'break-word',
+                                                color: message.user? '#000000' :'#FFFFFF',
+                                            }}
+                                            >
+                                            {message.text}
+                                        </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
+                            <form onSubmit={handleChatSubmit} style={{ display: 'flex' }}>
+                                <input type="text" name="chatInput" placeholder="Type a message..." style={{ flex: 1, padding: '8px', borderRadius: '10px', border: '1px solid #ccc', outline: 'none', fontSize: '14px' }} />
+                                <button type="submit" style={{ padding: '8px 16px', marginLeft: '10px', borderRadius: '10px', backgroundColor: '#009bdb', color: '#fff', border: 'none', cursor: 'pointer' }}>
+                                    Send
+                                </button>
+                            </form>
                         </div>
-                        <form onSubmit={handleChatSubmit} style={{ display: 'flex' }}>
-                            <input type="text" name="chatInput" placeholder="Type a message..." style={{ flex: 1, padding: '8px', borderRadius: '10px', border: '1px solid #ccc', outline: 'none', fontSize: '14px' }} />
-                            <button type="submit" style={{ padding: '8px 16px', marginLeft: '10px', borderRadius: '10px', backgroundColor: '#009bdb', color: '#fff', border: 'none', cursor: 'pointer' }}>
-                                Send
-                            </button>
-                        </form>
                     </div>
                 </div>
             </div>
