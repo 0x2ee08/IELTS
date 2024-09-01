429ecdd15f76570964a6810811256edc159c4e1f
0x2ee08 <1923012716718@thcsnamtuliem.edu.vn>
2024-09-01 23:36:54 +0700

    add comment

--- backend/routes/contestRoutes.js
+++ backend/routes/contestRoutes.js
@@ -19,6 +19,10 @@ router.post('/createContest', authenticateToken, async (req, res) => {
     const contestCollection = db.collection(`contest`);
 
     console.log(paragraphs)
+    //paragraph là toàn bộ đề (gồm 3 secontion, mỗi section có 3 bài...)
+    //xem file json t gửi trên discord để bt thêm)
+    //nhớ thêm 1 phần kiểm tra tất cả các từ trong paragraphs thuộc trình độ CEFR nào
+    // rồi add vào 1 cái mảng vocab nào đấy để lưu vào db
 
     res.json({"Status" : "Success"});
 });
