3d1d1799d21b94e0f1d391d3965e21f2c79b22dd
2019dasher <masterchune@gmail.com>
2024-09-04 13:47:46 +0700

    add get note

--- frontend/app/components/tedtalk/tedvideodetail.tsx
+++ frontend/app/components/tedtalk/tedvideodetail.tsx
@@ -211,8 +211,8 @@ const TedVideoDetail: React.FC = () => {
     };
 
     const opts = {
-      height: '426px',
-      width: '100%',
+      height: '676',
+      width: '1202',
       playerVars: {
         autoplay: 1,
       },
@@ -226,38 +226,35 @@ const TedVideoDetail: React.FC = () => {
 
     return (
         <div className="flex flex-col min-h-screen">
-            <div className="flex flex-col lg:flex-row justify-between space-y-8 lg:space-y-0 lg:space-x-8 px-8 lg:px-12 py-8">
-                {/* Left Column: 2/3 Width */}
-                    <div className="lg:w-2/3 space-y-8">
-                    {/* Left Column */}
-                    <div style={{ display: 'grid', gridGap: '20px', height:'100%' }}>
-                        {/* Video Player */}
-                        <div style={{ borderRadius: '10px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-                        <YouTube
-                            videoId={videoId}
-                            opts={opts}
-                            onReady={onReady}
-                            onStateChange={onStateChange}
-                        />
-                        </div>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 0.352518fr', gridGap: '40px', padding: '20px', boxSizing: 'border-box' }}>
+                {/* Left Column */}
+                <div style={{ display: 'grid', gridTemplateRows: '1fr 0.125fr 0.5fr', gridGap: '20px' }}>
+                    {/* Video Player */}
+                    <div style={{ borderRadius: '10px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                      <YouTube
+                        videoId={videoId}
+                        opts={opts}
+                        onReady={onReady}
+                        onStateChange={onStateChange}
+                      />
+                    </div>
 
-                        {/* Video Description */}
-                        <div style={{ padding: '10px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-                            <strong>{video.title}</strong>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '10px' }}>
-                                <span style={{ color: '#888' }}>Thời lượng: {convertDuration(video.duration)} | {dateString}</span>
-                                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', gap: '10px' }}>
-                                    <span style={{ display: 'flex', alignItems: 'center', color: '#009bdb' }}>
-                                        <img src={eyeIcon.src} alt="Views" style={{ width: '20px', height: '20px', marginRight: '5px' }} /> {video.views}
-                                    </span>
-                                    <span style={{ display: 'flex', alignItems: 'center', color: '#009bdb' }}>
-                                        <img src={heartIcon.src} alt="Likes" style={{ width: '20px', height: '20px', marginRight: '5px' }} /> {video.likes}
-                                    </span>
-                                </div>
+                    {/* Video Description */}
+                    <div style={{ padding: '10px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                        <strong>{video.title}</strong>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '10px' }}>
+                            <span style={{ color: '#888' }}>Thời lượng: {convertDuration(video.duration)} | {dateString}</span>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', gap: '10px' }}>
+                                <span style={{ display: 'flex', alignItems: 'center', color: '#009bdb' }}>
+                                    <img src={eyeIcon.src} alt="Views" style={{ width: '20px', height: '20px', marginRight: '5px' }} /> {video.views}
+                                </span>
+                                <span style={{ display: 'flex', alignItems: 'center', color: '#009bdb' }}>
+                                    <img src={heartIcon.src} alt="Likes" style={{ width: '20px', height: '20px', marginRight: '5px' }} /> {video.likes}
+                                </span>
                             </div>
                         </div>
+                    </div>
 
-<<<<<<< HEAD
                     {/* Personal Notes */}
                     <div style={{padding: '10px', position: 'relative', borderRadius: '10px',boxShadow: '0 0px 0px rgba(0, 0, 0, 0.1)'}}>
                         <div style={{
@@ -317,151 +314,97 @@ const TedVideoDetail: React.FC = () => {
                                     <p>{savedNotes}</p>
                                 </div>
                             )}
-=======
-                        {/* Personal Notes */}
-                        <div style={{padding: '10px', position: 'relative', borderRadius: '10px',boxShadow: '0 0px 0px rgba(0, 0, 0, 0.1)'}}>
-                            <div style={{
-                                backgroundImage: 'url(https://s3-alpha-sig.figma.com/img/6fcf/2061/ae4b0cd5da8149b7f430a7ad4198c1b0?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KlSDz23WBPGx-swvJTt6vSePBWBqPKZNh1XqvY61GynewD02VQ~~cNFXSmWsZmJH9Qq3P97PObKGr4louRFZPxUl2v4M~5TSgXrluDvZXaXYBzG3NiEi9MFNDlJteRirsvjTMufBgPLxfYmLUJJ4aGPQ-woEdOxNNYQdJSpftDB-Y4WFAZWyDqofTeFqXd-puaomrTMV-5bENz4yXrCWfe2HslcPuXmZXxHClt5weuijeFSsppeS6Mmaiq2GOun8JIoNQcxmYI~~y15DbRXBYnMlA45fhIBmmoH4woNUt5O7mTxmqrU9Jmhqu2C~~mBOBQElahpP38RYxJqDH2mctA__)',
-                                backgroundSize: 'contain',
-                                borderRadius: '10px',
-                                opacity: 0.2,
-                                position: 'absolute',
-                                top: 0,
-                                left: 0,
-                                right: 0,
-                                bottom: 0,
-                                zIndex: 1
-                            }}></div>
-                            <div style={{
-                                position: 'relative',
-                                height: '205px',
-                                zIndex: 2
-                            }}>
-                                <h3 style={{
-                                    fontSize: '25px',
-                                    fontWeight: 'bold',
-                                    color: '#00B4D8'
-                                }}>Personal note:</h3>
-                                <textarea
-                                    value={notes}
-                                    onChange={handleNoteChange}
-                                    style={{
-                                        width: '100%',
-                                        height: '150px',
-                                        border: 'none',
-                                        outline: 'none',
-                                        padding: '10px',
-                                        borderRadius: '10px',
-                                        boxSizing: 'border-box',
-                                        backgroundColor: 'transparent',
-                                        fontFamily: 'Arial, sans-serif',
-                                        fontSize: '14px',
-                                        lineHeight: '1.5',
-                                        color: '#333',
-                                        resize: 'none',
-                                    }}
-                                    placeholder="Write your notes here..."
-                                ></textarea>
-                                <button className="text-blue-400 hover:underline"
-                                    onClick = {() => handleSaveNote(videoId,notes)}
-                                >
-                                    Save note
-                                </button>
-                            </div>
->>>>>>> 97639c48b61483bc033d7a7499357ba95469171d
                         </div>
                     </div>
                 </div>
-                {/* Right Column: 1/3 Width */}
-                <div className="lg:w-1/3 space-y-8">
-                    {/* Right Column */}
-                    <div style={{ display: 'grid', gridTemplateRows: '1fr 0.675fr', gridGap: '20px' }}>
-                        {/* Transcript */}
-                        <div style={{
-                            height: '426px',
-                            width: '100%',
-                            padding: '20px',
-                            backgroundColor: 'white',
-                            borderRadius: '10px',
-                            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                            border: '1px solid #d9d9d9',
-                        }}>
-                            <h1 style={{
-                                fontWeight: 'bold',
-                                textAlign: 'center' as const,
-                                color: '#0077b6',
-                                fontSize: '24px',
-                                margin: '0',
-                            }}>TRANSCRIPT</h1>
-                            <button onClick={toggleTranscriptVisibility} className="toggle-button text-blue-500">
-                                {isTranscriptVisible ? "Hide Transcript" : "Show Transcript"}
-                            </button>
-                            <div className="border-b border-blue-500 mt-2"></div>
-                            <div 
-                                ref={transcriptRef}
-                                style={{
-                                color: '#555',
-                                fontSize: '14px',
-                                lineHeight: '1.6',
-                                maxHeight: '326px',
-                                overflowY: 'auto',
-                                }}
-                            >
-                            {filteredTranscript.map((item, index) => (
-                                <div key={index}>
-                                    {isTranscriptVisible ? (
-                                        <p>
-                                            <strong>
-                                                {convertSecondsToReadable(Math.floor(item.offset))}:{' '} 
-                                            </strong> 
-                                            {decodeHtmlEntities(item.text)} 
-                                        </p>
-                                    ) : (
-                                        <p style={{ margin: 0 }}>
-                                            <strong>
-                                                {convertSecondsToReadable(Math.floor(item.offset))}:{' '}  
-                                            </strong> 
-                                                {'.'.repeat(item.text.length)}
-                                        </p>
-                                    )}
-                                    
-                                </div>
-                            ))}
-                            </div>
-                        </div>
 
-                        {/* Chat Bot */}
-                        <div style={{ padding: '1px', backgroundColor: '#0077B6', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column', width:'100%', height:'320px'}}>
-                            <h3 style={{ padding: '10px', fontSize: '20px', fontWeight: 'bold', color: '#FFFFFF' }}>AI chat bot BETA</h3>
-                            <div style={{ padding: '10px', backgroundColor: '#FFFFFF', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column', width:'100%', height: '300px', overflow: 'hidden' }}>
-                                <div style={{ flex: 1, overflowY: 'auto' as const, marginBottom: '10px', paddingRight: '10px', wordWrap: 'break-word' }}>
-                                    {messages.map((message, index) => (
-                                        <div key={index} style={{ textAlign: message.user ? 'right' : 'left', marginBottom: '10px' }}>
-                                            <div
-                                                style={{
-                                                    display: 'inline-block',
-                                                    padding: '8px',
-                                                    borderRadius: '10px',
-                                                    background: message.user ? '#E5E5E5' : '#00B4D8',
-                                                    maxWidth: '100%',
-                                                    maxHeight:'100%',
-                                                    wordWrap: 'break-word',
-                                                    color: message.user ? '#000000' : '#FFFFFF',
-                                                }}
-                                            >
-                                                {message.text}
-                                            </div>
-                                        </div>
-                                    ))}
-                                    <div ref={messagesEndRef} />
-                                </div>
-                                <form onSubmit={handleChatSubmit} style={{ display: 'flex' }}>
-                                    <input type="text" name="chatInput" placeholder="Type a message..." style={{ flex: 1, padding: '8px', borderRadius: '10px', border: '1px solid #ccc', outline: 'none', fontSize: '14px' }} />
-                                    <button type="submit" style={{ padding: '8px 16px', marginLeft: '10px', borderRadius: '10px', backgroundColor: '#009bdb', color: '#fff', border: 'none', cursor: 'pointer' }}>
-                                        Send
-                                    </button>
-                                </form>
+                {/* Right Column */}
+                <div style={{ display: 'grid', gridTemplateRows: '1fr 0.675fr', gridGap: '20px' }}>
+                    {/* Transcript */}
+                    <div style={{
+                        height: '100%',
+                        width: '100%',
+                        padding: '20px',
+                        backgroundColor: 'white',
+                        borderRadius: '10px',
+                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                        border: '1px solid #d9d9d9',
+                    }}>
+                        <h1 style={{
+                            fontWeight: 'bold',
+                            textAlign: 'center' as const,
+                            color: '#0077b6',
+                            fontSize: '24px',
+                            margin: '0',
+                        }}>TRANSCRIPT</h1>
+                        <button onClick={toggleTranscriptVisibility} className="toggle-button text-blue-500">
+                            {isTranscriptVisible ? "Hide Transcript" : "Show Transcript"}
+                        </button>
+                        <div className="border-b border-blue-500 mt-2"></div>
+                        <div 
+                            ref={transcriptRef}
+                            style={{
+                              color: '#555',
+                              fontSize: '14px',
+                              lineHeight: '1.6',
+                              maxHeight: '400px',
+                              overflowY: 'auto',
+                            }}
+                        >
+                          {filteredTranscript.map((item, index) => (
+                            <div key={index}>
+
+                                {isTranscriptVisible ? (
+                                    <p>
+                                        <strong>
+                                            {convertSecondsToReadable(Math.floor(item.offset))}:{' '} 
+                                        </strong> 
+                                        {decodeHtmlEntities(item.text)} 
+                                    </p>
+                                ) : (
+                                    <p style={{ margin: 0 }}>
+                                        <strong>
+                                            {convertSecondsToReadable(Math.floor(item.offset))}:{' '}  
+                                        </strong> 
+                                            {'.'.repeat(item.text.length)}
+                                    </p>
+                                )}
+                                 
                             </div>
+                          ))}
+                        </div>
+                    </div>
+                    {/* Chat Bot */}
+                    <div style={{ padding: '1px', backgroundColor: '#0077B6', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column', width:'100%', height:'100%'}}>
+                        <h3 style={{ padding: '10px', fontSize: '20px', fontWeight: 'bold', color: '#FFFFFF' }}>AI chat bot BETA</h3>
+                        <div style={{ padding: '10px', backgroundColor: '#FFFFFF', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column', width:'100%', height: '100%', overflow: 'hidden' }}>
+                            <div style={{ flex: 1, overflowY: 'auto' as const, marginBottom: '10px', paddingRight: '10px', wordWrap: 'break-word' }}>
+                                {messages.map((message, index) => (
+                                    <div key={index} style={{ textAlign: message.user ? 'right' : 'left', marginBottom: '10px' }}>
+                                        <div
+                                            style={{
+                                                display: 'inline-block',
+                                                padding: '8px',
+                                                borderRadius: '10px',
+                                                background: message.user ? '#E5E5E5' : '#00B4D8',
+                                                maxWidth: '100%',
+                                                maxHeight:'100%',
+                                                wordWrap: 'break-word',
+                                                color: message.user ? '#000000' : '#FFFFFF',
+                                            }}
+                                        >
+                                            {message.text}
+                                        </div>
+                                    </div>
+                                ))}
+                                <div ref={messagesEndRef} />
+                            </div>
+                            <form onSubmit={handleChatSubmit} style={{ display: 'flex' }}>
+                                <input type="text" name="chatInput" placeholder="Type a message..." style={{ flex: 1, padding: '8px', borderRadius: '10px', border: '1px solid #ccc', outline: 'none', fontSize: '14px' }} />
+                                <button type="submit" style={{ padding: '8px 16px', marginLeft: '10px', borderRadius: '10px', backgroundColor: '#009bdb', color: '#fff', border: 'none', cursor: 'pointer' }}>
+                                    Send
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
