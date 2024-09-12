2a7636cdde3a06ecc40efc7c5b25c1977f5ddded
anHiep <HIE2Pbrizzily@gmail.com>
2024-09-13 02:58:02 +0700

    add check pronounce (audioBlob is not working =(()

--- frontend/app/components/problem/speaking/component/result.tsx
+++ frontend/app/components/problem/speaking/component/result.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import config from '../../../../config';
 import { Accordion, AccordionItem } from '@nextui-org/react';
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 export interface QuestionGeneral {
     type: string;
@@ -32,7 +33,7 @@ interface TranscriptData {
 interface ResultItem {
     [key: string]: {
         data: TranscriptData;
-        audioBlob: any;
+        audioBlob: Blob;
     };
 }
 
@@ -44,8 +45,10 @@ interface UserAnswer {
 const ResultPage: React.FC<ResultPageProps> = ({ task, task_id, problem_id }) => {
     const [answerArray, setAnswerArray] = useState<ResultItem[]>([]);
     const username = localStorage.getItem('username');
+    const STScoreAPIKey = 'rll5QsTiv83nti99BW6uCmvs9BDVxSB39SVFceYb';
 
     const hasInitialize = useRef(false);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
         if (!hasInitialize.current) {
@@ -65,19 +68,126 @@ const ResultPage: React.FC<ResultPageProps> = ({ task, task_id, problem_id }) =>
             body: JSON.stringify({ problem_id }),
         });
         const result = await response.json();
-        console.log('API Result:', result); // Log the result to inspect the structure
         const userAnswer = result.answer;
         if (userAnswer && Array.isArray(userAnswer.result)) {
-            // setAnswerArray(userAnswer.result);
             setAnswerArray([]);
-            for(let i=0; i<userAnswer.result.length; i++) {
-                console.log(task_id);
-                if(userAnswer.result[i].task_id === task_id)
+            for (let i = 0; i < userAnswer.result.length; i++) {
+                if (userAnswer.result[i].task_id === task_id)
                     setAnswerArray(prevAnswerArray => [...prevAnswerArray, userAnswer.result[i]]);
             }
         } else {
             setAnswerArray([]);
         }
+    };
+
+    const playSpeechFromWord = async (text: string) => {
+        await fetch(`${config.API_PRONOUNCE_BASE_URL}/getAudioFromText`, {
+            method: "post",
+            body: JSON.stringify({ "text": text }),
+            headers: { "X-Api-Key": STScoreAPIKey }
+        });
+
+        const audio = new Audio('../../../../backend_pronounce/audio.wav');
+        audio.play()
+            .then(() => console.log('Audio is playing'))
+            .catch((error) => console.error('Error playing audio:', error));
+    };
+
+    const replayAudio = (audioBlob: Blob, startTime: number, endTime: number) => {
+
+        if (!(audioBlob instanceof Blob)) {
+            console.log(audioBlob)
+            console.log('Invalid audioBlob: Not a Blob object');
+            return;
+        }
+        const audioURL = URL.createObjectURL(audioBlob);
+        const audio = new Audio(audioURL);
+
+        // Ensure startTime and endTime are finite numbers
+        const start = Number(startTime);
+        const end = Number(endTime);
+
+        // Ensure start and end are finite numbers and round to 4 decimal places
+        const roundedStartTime = parseFloat(start.toFixed(4));
+        const roundedEndTime = parseFloat(end.toFixed(4));
+
+        if (roundedEndTime > roundedStartTime) {
+            audio.currentTime = roundedStartTime;
+            audio.play()
+                .then(() => {
+                    console.log('Audio is playing');
+                    const durationInSeconds = roundedEndTime - roundedStartTime;
+                    const durationInMs = Math.round(durationInSeconds * 1000);
+
+                    setTimeout(() => {
+                        audio.pause();
+                        URL.revokeObjectURL(audioURL);
+                    }, durationInMs);
+                })
+                .catch((error) => console.error('Error playing audio:', error));
+        } else {
+            console.error('Invalid playback range: endTime must be greater than startTime.');
+        }
+    };
+
+    const getColoredMatchedTranscript = (
+                    matchedTranscript: string,
+                    is_letter_correct_all_words: string,
+                    real_transcripts_ipa: string,
+                    real_start_time: number[],
+                    real_end_time: number[],
+                    audioBlob: Blob,) => {
+        const currentTextWords = matchedTranscript.split(' ');
+        const lettersOfWordAreCorrect = is_letter_correct_all_words.split(' ');
+        const realTranscript = real_transcripts_ipa.split(' ');
+        const start_time = real_start_time;
+        let end_time = real_end_time;
+        let coloredWords = [];
+
+        for (let word_idx = 0; word_idx < currentTextWords.length; word_idx++) {
+            const currentWord = currentTextWords[word_idx];
+            let wordTemp = [];
+
+            for (let letter_idx = 0; letter_idx < currentWord.length; letter_idx++) {
+                const letter_is_correct = lettersOfWordAreCorrect[word_idx][letter_idx] === '1';
+                const color_letter = letter_is_correct ? 'green' : 'red';
+                wordTemp.push(
+                    <span key={`${word_idx}-${letter_idx}`} style={{ color: color_letter }}>
+                        {currentWord[letter_idx]}
+                    </span>
+                );
+            }
+
+            coloredWords.push(
+                <div
+                    key={`word-container-${word_idx}`}
+                    style={{
+                        display: 'inline-block',
+                        textAlign: 'center',
+                        marginRight: '10px',
+                    }}
+                >
+                    <span
+                        onClick={() => replayAudio(audioBlob, start_time[word_idx], end_time[word_idx])}
+                        style={{ cursor: 'pointer', display: 'block' }}
+                    >
+                        {wordTemp}
+                    </span>
+
+                    <span
+                        onClick={() => {
+                            playSpeechFromWord(currentTextWords[word_idx]);
+                        }}
+                        style={{ cursor: 'pointer', display: 'block', color: 'gray' }}
+                    >
+                        {realTranscript[word_idx]}
+                    </span>
+
+                </div>
+            );
+        }
+
+        return coloredWords;
     };
 
     return (
@@ -92,12 +202,20 @@ const ResultPage: React.FC<ResultPageProps> = ({ task, task_id, problem_id }) =>
                                 title={`Submission ${idx + 1} of user ${username}`}
                             >
                                 {Object.entries(item).slice(0, -2).map(([key, value]) => (
-                                    <div className='flex flex-col mb-4' key={key}>
-                                        <div className='mb-4'>
+                                    <div className='flex-col mb-4' key={key}>
+                                        <div className='text-left'>
                                             {task.questions[parseInt(key)] || `Question ${parseInt(key) + 1}`}
                                         </div>
-                                        <div>
-                                            {value?.data?.matched_transcripts || 'No transcript available'}
+                                        <div className='text-right'>
+                                            {/* {value?.data?.matched_transcripts || 'No transcript available'} */}
+                                            {getColoredMatchedTranscript(
+                                                value?.data?.matched_transcripts,
+                                                value?.data?.is_letter_correct_all_words,
+                                                value?.data?.real_transcripts_ipa,
+                                                value?.data?.start_time,
+                                                value?.data?.end_time,
+                                                value?.audioBlob,
+                                            )}
                                         </div>
                                     </div>
                                 ))}
