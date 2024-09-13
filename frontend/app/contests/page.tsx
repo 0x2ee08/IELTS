b0263e25a126c971681908c7a57a6a44a5de6de6
nhtd <nguyenhoangtiendata3@gmail.com>
2024-09-13 14:57:30 +0700

    fix main contest p2

--- frontend/app/contests/page.tsx
+++ frontend/app/contests/page.tsx
@@ -50,12 +50,12 @@ const ContestPage: React.FC = () => {
         <>
             <Header />
 
-            <section style={{textAlign:"center",alignItems:"center",marginLeft:"1%",marginRight:"5%",paddingRight:"5%",marginTop:"25px",justifyContent:"space-between"}}>
-                <h2>Upcoming Contest</h2>
-                <div style={{margin:"10px",padding:"10px"}}>
+            <section style={{textAlign:"center",alignItems:"center",marginLeft:"1%",marginRight:"5%",paddingLeft:'5%',paddingRight:'5%',marginTop:"25px",justifyContent:"space-between"}}>
+                <h2 style={{fontSize:'25px',fontWeight:'bold'}}>Upcoming Contest</h2>
+                <div style={{margin:"10px"}}>
                 {upcomingContest ? (
                         <table style={{margin:"0 auto",borderSpacing:"0 10px",borderCollapse:"separate"}}>
-                            <tr style={{borderBottom:"1px solid #000"}}>
+                            <tr style={{borderBottom:"1px solid #000",backgroundColor:"#E8E8E8"}}>
                                 <th>Contest</th>
                                 <th>Author</th>
                                 <th>Start</th>
@@ -65,12 +65,17 @@ const ContestPage: React.FC = () => {
                             </tr>
                             {upcomingContest.map((contest, cnt) =>
                                 <tr key={contest.id} style={{borderBottom:"1px solid #000",background:cnt%2==0?"#E2FAFF":"#CAF0F8"}}>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.problemName}<br/>({contest.access})</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.created_by}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.startTime}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.endTime}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.registerUser}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}><button type='button'>Register</button></td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'40%'}}>{contest.problemName}<br/>({contest.access})</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'5%'}}>{
+                                        <Link href={`/loader/profile?id=${contest.created_by}`}>
+                                            <span className="text-blue-600 hover:underline cursor-pointer" 
+                                            style={{fontWeight:'bold'}}>{contest.created_by}</span>
+                                        </Link>}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'15%'}}>{contest.startTime}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'15%'}}>{contest.endTime}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'5%'}}>{contest.registerUser}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'10%'}}>
+                                        <button className="text-blue-600 hover:underline cursor-pointer">Register</button></td>
                                 </tr>
                             )}
                         </table>
@@ -80,12 +85,12 @@ const ContestPage: React.FC = () => {
                 </div>
             </section>
             
-            <section style={{textAlign:"center",alignItems:"center",marginLeft:"1%",marginRight:"5%",paddingRight:"5%",marginTop:"25px",justifyContent:"space-between"}}>
-                <h2>Past Contest</h2>
+            <section style={{textAlign:"center",alignItems:"center",marginLeft:"1%",marginRight:"5%",paddingLeft:'5%',paddingRight:'5%',marginTop:"25px",justifyContent:"space-between"}}>
+                <h2 style={{fontSize:'25px', fontWeight:'bold'}}>Past Contest</h2>
                 <div style={{margin:"10px",padding:"10px"}}>
                 {pastContest ? (
                         <table style={{margin:"0 auto",borderSpacing:"0 10px",borderCollapse:"separate"}}>
-                            <tr style={{borderBottom:"1px solid #000"}}>
+                            <tr style={{borderBottom:"1px solid #000",backgroundColor:"#E8E8E8"}}>
                                 <th>Contest</th>
                                 <th>Author</th>
                                 <th>Start</th>
@@ -95,12 +100,17 @@ const ContestPage: React.FC = () => {
                             </tr>
                             {pastContest.map((contest,cnt) =>
                                 <tr key={contest.id} style={{borderBottom:"1px solid #000",background:cnt%2==0?"#E2FAFF":"#CAF0F8"}}>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.problemName}<br/>({contest.access})</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.created_by}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.startTime}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.endTime}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}>{contest.registerUser}</td>
-                                    <td style={{padding:"10px",textAlign:"center"}}><Link href={`/contests/${contest.id}`}><button>Join</button></Link></td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'40%'}}>{contest.problemName}<br/>({contest.access})</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'5%'}}>{
+                                        <Link href={`/loader/profile?id=${contest.created_by}`}>
+                                            <span className="text-blue-600 hover:underline cursor-pointer bold" 
+                                            style={{fontWeight:'bold'}}>{contest.created_by}</span>
+                                        </Link>}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'15%'}}>{contest.startTime}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'15%'}}>{contest.endTime}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'5%'}}>{contest.registerUser}</td>
+                                    <td style={{padding:"10px",textAlign:"center",width:'10%'}}><Link href={`/contests/${contest.id}`}>
+                                        <button className="text-blue-600 hover:underline cursor-pointer">Join</button></Link></td>
                                 </tr>
                             )}
                         </table>
