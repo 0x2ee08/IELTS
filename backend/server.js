38e377dd1ea4e46caff59a6bb2b548cfb3eac64c
cnn008 <chuongnguyenng@gmail.com>
2024-09-24 22:55:26 +0700

    small update flashcards

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use('/api', graderWritingTask1Academic);
 const flashcardsRoutes = require('./routes/flashcards');
 app.use('/api/flashcards', flashcardsRoutes);
 
-const getallfcRoutes = require('./routes/getAllFlashcards');
+const getallfcRoutes = require('./routes/getallflashcards');
 app.use('/api/getAllFlashcards', getallfcRoutes);
 
 app.listen(PORT, () => {
