249e85ba122c1549e65678e6b951057cf496c4e5
pitou122 <nphong131022@gmail.com>
2024-09-27 22:09:32 +0700

    speaking render

--- frontend/app/results/[submissionID]/SpeakingRender.tsx
+++ frontend/app/results/[submissionID]/SpeakingRender.tsx
@@ -45,18 +45,57 @@ export default function SpeakingRender() {
 
     console.log(submission);
   return (
-    <>
-        Speaking page : {submissionID}
-        <br />
+    <div className='flex flex-col min-h-screen p-4 ml-16 mr-16'>
+        
         {submission ? (
             <>
-                Only owners can access to the audio file by going to contest page by click this <Link href={`/contests/${submission.cid}`}>Link</Link> 
+                <div className='flex flex-col justify-between bg-white border border-gray-300 rounded-lg shadow-md p-8'>
+                    <Link 
+                    href={`/contests/${submission.cid}`}
+                    className='font-black text-4xl'
+                    >
+                        {submission.contest_title}
+                    </Link> 
+                    <div>
+                    <span>By: </span>
+                    <Link href={`/loader/profile?id=${submission.submit_by}`}>
+                        <span className="text-blue-600 hover:underline cursor-pointer">{submission.submit_by}</span>
+                    </Link>
+                    <span> ,{new Date(submission.submit_time).toLocaleString()}</span>
+                    </div>
+                </div>
+                <ul>
+                    {submission.questions.map((question, index) => {
+                        const res = submission.result[index];
+                        return (
+                            <div
+                            key={index}
+                            className='flex flex-col justify-between bg-white border border-gray-300 rounded-lg shadow-md p-8 mt-4'
+                            >
+                                <h3>Question {index + 1}:</h3>
+                                <p>{question}</p>
+                                {res && res.feedback && ( 
+                                    <>
+                                    <h3>Your response: {res.data.matched_transcripts}</h3>
+                                    <div>
+                                        <p><strong>Fluency:</strong> {res.feedback.fluency}</p>
+                                        <p><strong>Grammar:</strong> {res.feedback.grammar}</p>
+                                        <p><strong>Lexical:</strong> {res.feedback.lexical}</p>
+                                        <p><strong>Response:</strong> {res.feedback.response}</p>
+                                        <p><strong>Total:</strong> {res.feedback.total}</p>
+                                    </div>
+                                    </>
+                                )}
+                        </div>
+                        );
+                    })}
+                </ul>
             </>
+
+            
         ) : (
             <p>Loading submission data...</p>
         )}
-        {/* cid: {submission.cid}; */}
-        
-    </>
+    </div>
   );
 }
