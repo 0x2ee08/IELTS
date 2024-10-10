8a5c67cd1789bec9ffd956aaa7c8b64facf92527
D <141933570+Spectrum3563@users.noreply.github.com>
2024-10-10 17:15:22 +0700

    small change

--- frontend/app/contests/page.tsx
+++ frontend/app/contests/page.tsx
@@ -10,6 +10,7 @@ import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import Link from 'next/link';
 import { format } from 'path';
+import './style.css';
 
 interface Contest {
     id: string;
@@ -92,106 +93,132 @@ const ContestPage: React.FC = () => {
     return (
         <>
             <Header />
-            <section style={{textAlign:"center",alignItems:"center",paddingLeft:'5%',paddingRight:'5%',marginTop:"25px",justifyContent:"space-between"}}>
-                <div style={{padding:'3px', paddingBottom:'5px', borderRadius: '10px'}}>
-                    <h2 style={{fontSize:'25px',fontWeight:'bold', padding:'30px'}}>Ongoing Contest</h2>
+            <section className="contest-section">
+                <div className="contest-container">
+                    <h2 className="contest-title">Ongoing Contest</h2>
                     {onGoingContest ? (
-                            <table style={{margin:"0 auto",borderCollapse:"collapse",border:"1px solid black",width:'100%'}}>
-                                <tr style={{border:'1px solid #e1e1e1',backgroundColor:"white"}}>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Contest</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Author</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Start</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>End</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Participant</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}></th>
+                        <table className="contest-table">
+                            <thead>
+                                <tr>
+                                    <th>Contest</th>
+                                    <th>Author</th>
+                                    <th>Start</th>
+                                    <th>End</th>
+                                    <th>Participants</th>
+                                    <th></th>
                                 </tr>
-                                {onGoingContest.map((contest, cnt) =>
-                                    <tr key={contest.id} style={{background:cnt%2==0?"#F0F0F0":"white"}}>
-                                        <td style={{padding:'10px',textAlign:"center",width:'40%',border:'1px solid #e1e1e1'}}>{contest.problemName}<br/>({contest.access})</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>{
+                            </thead>
+                            <tbody>
+                                {onGoingContest.map((contest, cnt) => (
+                                    <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
+                                        <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
+                                        <td className="contest-author">
                                             <Link href={`/loader/profile?id=${contest.created_by}`}>
-                                                <span className="text-blue-600 hover:underline cursor-pointer bold" 
-                                                style={{fontWeight:'bold'}}>{contest.created_by}</span>
-                                            </Link>}</td>
-                                            <td style={{padding:'10px',textAlign:"center",width:'15%',border:'1px solid #e1e1e1'}}>{formatDate(contest.startTime)}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'15%',border:'1px solid #e1e1e1'}}>{formatDate(contest.endTime)}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>{contest.registerUser}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>
-                                        {contest.registered === true ? <Link href={`/contests/${contest.id}`}><button className="text-blue-600 hover:underline cursor-pointer">Join</button></Link> : <button onClick={() => handleRegister(contest.id)} className="text-blue-600 hover:underline cursor-pointer">Register</button>}
+                                                <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
+                                            </Link>
+                                        </td>
+                                        <td>{formatDate(contest.startTime)}</td>
+                                        <td>{formatDate(contest.endTime)}</td>
+                                        <td>{contest.registerUser}</td>
+                                        <td>
+                                            {contest.registered ? (
+                                                <Link href={`/contests/${contest.id}`}>
+                                                    <button className="text-blue-600 hover:underline cursor-pointer">Join</button>
+                                                </Link>
+                                            ) : (
+                                                <button onClick={() => handleRegister(contest.id)} className="text-blue-600 hover:underline cursor-pointer">Register</button>
+                                            )}
                                         </td>
                                     </tr>
-                                )}
-                            </table>
+                                ))}
+                            </tbody>
+                        </table>
                     ) : (
                         <p>Loading...</p>
                     )}
                 </div>
             </section>
-            <section style={{textAlign:"center",alignItems:"center",paddingLeft:'5%',paddingRight:'5%',marginTop:"25px",justifyContent:"space-between"}}>
-                <div style={{padding:'3px', paddingBottom:'5px', borderRadius: '10px'}}>
-                    <h2 style={{fontSize:'25px',fontWeight:'bold', padding:'30px'}}>Upcoming Contest</h2>
+
+            <section className="contest-section">
+                <div className="contest-container">
+                    <h2 className="contest-title">Upcoming Contest</h2>
                     {upcomingContest ? (
-                            <table style={{margin:"0 auto",borderCollapse:"collapse",border:"1px solid black",width:'100%'}}>
-                                <tr style={{border:'1px solid #e1e1e1',backgroundColor:"white"}}>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Contest</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Author</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Start</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>End</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Participant</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}></th>
+                        <table className="contest-table">
+                            <thead>
+                                <tr>
+                                    <th>Contest</th>
+                                    <th>Author</th>
+                                    <th>Start</th>
+                                    <th>End</th>
+                                    <th>Participants</th>
+                                    <th></th>
                                 </tr>
-                                {upcomingContest.map((contest, cnt) =>
-                                    <tr key={contest.id} style={{background:cnt%2==0?"#F0F0F0":"white"}}>
-                                        <td style={{padding:'10px',textAlign:"center",width:'40%',border:'1px solid #e1e1e1'}}>{contest.problemName}<br/>({contest.access})</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>{
+                            </thead>
+                            <tbody>
+                                {upcomingContest.map((contest, cnt) => (
+                                    <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
+                                        <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
+                                        <td className="contest-author">
                                             <Link href={`/loader/profile?id=${contest.created_by}`}>
-                                                <span className="text-blue-600 hover:underline cursor-pointer bold" 
-                                                style={{fontWeight:'bold'}}>{contest.created_by}</span>
-                                            </Link>}</td>
-                                            <td style={{padding:'10px',textAlign:"center",width:'15%',border:'1px solid #e1e1e1'}}>{formatDate(contest.startTime)}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'15%',border:'1px solid #e1e1e1'}}>{formatDate(contest.endTime)}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>{contest.registerUser}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>
-                                            {contest.registered === true ? <>Already register</> : <button onClick={() => handleRegister(contest.id)} className="text-blue-600 hover:underline cursor-pointer">Register</button>}
+                                                <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
+                                            </Link>
+                                        </td>
+                                        <td>{formatDate(contest.startTime)}</td>
+                                        <td>{formatDate(contest.endTime)}</td>
+                                        <td>{contest.registerUser}</td>
+                                        <td>
+                                            {contest.registered ? (
+                                                <div className='text-gray-500'>Already registered</div>
+                                            ) : (
+                                                <button onClick={() => handleRegister(contest.id)} className="text-blue-600 hover:underline cursor-pointer">Register</button>
+                                            )}
                                         </td>
                                     </tr>
-                                )}
-                            </table>
+                                ))}
+                            </tbody>
+                        </table>
                     ) : (
                         <p>Loading...</p>
                     )}
                 </div>
             </section>
-            <div style={{padding:'10px'}}></div>
-            <section style={{textAlign:"center",alignItems:"center",paddingLeft:'5%',paddingRight:'5%',marginTop:"25px",justifyContent:"space-between"}}>
-                <div style={{padding:'3px', paddingBottom:'5px', borderRadius: '10px'}}>
-                    <h2 style={{fontSize:'25px', fontWeight:'bold', padding:'30px'}}>Past Contest</h2>
+
+            <section className="contest-section">
+                <div className="contest-container">
+                    <h2 className="contest-title">Past Contest</h2>
                     {pastContest ? (
-                            <table style={{margin:"0 auto",borderCollapse:"collapse",border:"1px solid black",width:'100%'}}>
-                                <tr style={{border:'1px solid #e1e1e1',backgroundColor:"white"}}>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Contest</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Author</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Start</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>End</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}>Participant</th>
-                                    <th style={{border:'1px solid #e1e1e1'}}></th>
+                        <table className="contest-table">
+                            <thead>
+                                <tr>
+                                    <th>Contest</th>
+                                    <th>Author</th>
+                                    <th>Start</th>
+                                    <th>End</th>
+                                    <th>Participants</th>
+                                    <th></th>
                                 </tr>
-                                {pastContest.map((contest,cnt) =>
-                                    <tr key={contest.id} style={{background:cnt%2==0?"#F0F0F0":"white"}}>
-                                        <td style={{padding:'10px',textAlign:"center",width:'40%',border:'1px solid #e1e1e1'}}>{contest.problemName}<br/>({contest.access})</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>{
+                            </thead>
+                            <tbody>
+                                {pastContest.map((contest, cnt) => (
+                                    <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
+                                        <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
+                                        <td className="contest-author">
                                             <Link href={`/loader/profile?id=${contest.created_by}`}>
-                                                <span className="text-blue-600 hover:underline cursor-pointer bold" 
-                                                style={{fontWeight:'bold'}}>{contest.created_by}</span>
-                                            </Link>}</td>
-                                            <td style={{padding:'10px',textAlign:"center",width:'15%',border:'1px solid #e1e1e1'}}>{formatDate(contest.startTime)}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'15%',border:'1px solid #e1e1e1'}}>{formatDate(contest.endTime)}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>{contest.registerUser}</td>
-                                        <td style={{padding:'10px',textAlign:"center",width:'10%',border:'1px solid #e1e1e1'}}>
-                                            <Link href={`/contests/${contest.id}`}><button className="text-blue-600 hover:underline cursor-pointer">Join</button></Link></td>
+                                                <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
+                                            </Link>
+                                        </td>
+                                        <td>{formatDate(contest.startTime)}</td>
+                                        <td>{formatDate(contest.endTime)}</td>
+                                        <td>{contest.registerUser}</td>
+                                        <td>
+                                            <Link href={`/contests/${contest.id}`}>
+                                                <button className="text-blue-600 hover:underline cursor-pointer">Join</button>
+                                            </Link>
+                                        </td>
                                     </tr>
-                                )}
-                            </table>
+                                ))}
+                            </tbody>
+                        </table>
                     ) : (
                         <p>Loading...</p>
                     )}
