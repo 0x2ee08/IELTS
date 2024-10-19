53fd3496f54055a28fa72b7b0614889feb6c4382
D <141933570+Spectrum3563@users.noreply.github.com>
2024-10-19 20:28:50 +0700

    contest page

--- frontend/app/contests/page.tsx
+++ frontend/app/contests/page.tsx
@@ -78,155 +78,185 @@ const ContestPage: React.FC = () => {
         // Convert to UTC+7 timezone
         const timeInUTC7 = dayjs(isoString).tz('Asia/Bangkok');
 
-        // Format date and time
-        const formattedDate = timeInUTC7.format('DD/MMM/YYYY');
-        const formattedTime = timeInUTC7.format('HH:mm');
-    
-        return (
-                <>
-                    {formattedDate} {formattedTime}
-                    <span style={{ verticalAlign: 'super', fontSize: '10px' }}>UTC+7</span>
-                </>
-            );
+        const sufday = (day: number) => {
+            if (day >= 11 && day <= 13) return 'th';
+            switch (day % 10) {
+                case 1: return 'st';
+                case 2: return 'nd';
+                case 3: return 'rd';
+                default: return 'th';
+            }
         };
+
+        const day = timeInUTC7.format('DD');
+        const formattedDate = timeInUTC7.format('MMM YYYY');
+        const formattedTime = timeInUTC7.format('HH:mm');
+        
+        if (day === "Invalid Date") return (
+            <>
+                <div className='text-red-500 font-normal'>Invalid date</div>
+            </>
+        );
+
+        return (
+            <>
+                {day}<sup>{sufday(timeInUTC7.date())}</sup> {formattedDate} {formattedTime}
+                <span style={{ verticalAlign: 'super', fontSize: '10px' }}>UTC+7</span>
+            </>
+        );
+    };
 
     return (
         <>
-            <Header />
-            <section className="contest-section">
-                <div className="contest-container">
-                    <h2 className="contest-title">Ongoing Contest</h2>
-                    {onGoingContest ? (
-                        <table className="contest-table">
-                            <thead>
-                                <tr>
-                                    <th>Contest</th>
-                                    <th>Author</th>
-                                    <th>Start</th>
-                                    <th>End</th>
-                                    <th>Participants</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {onGoingContest.map((contest, cnt) => (
-                                    <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
-                                        <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
-                                        <td className="contest-author">
-                                            <Link href={`/loader/profile?id=${contest.created_by}`}>
-                                                <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
-                                            </Link>
-                                        </td>
-                                        <td>{formatDate(contest.startTime)}</td>
-                                        <td>{formatDate(contest.endTime)}</td>
-                                        <td>{contest.registerUser}</td>
-                                        <td>
-                                            {contest.registered ? (
-                                                <Link href={`/contests/${contest.id}`}>
-                                                    <button className="text-blue-600 hover:underline cursor-pointer">Join</button>
-                                                </Link>
-                                            ) : (
-                                                <button onClick={() => handleRegister(contest.id)} className="text-blue-600 hover:underline cursor-pointer">Register</button>
-                                            )}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-            </section>
+    <Header />
+    <section className="contest-section">
+        <div className="contest-container">
+            <h2 className="contest-title">Ongoing Contest</h2>
+            {onGoingContest ? onGoingContest.length ? (
+                <table className="contest-table">
+                    <thead>
+                        <tr>
+                            <th>Contest</th>
+                            <th>Author</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th>Participants</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {onGoingContest.map((contest, cnt) =>  (
+                            <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
+                                <td className="contest-name">
+                                    {contest.problemName}<br />({contest.access})
+                                </td>
+                                <td className="contest-author">
+                                    <Link href={`/loader/profile?id=${contest.created_by}`}>
+                                        <span className="text-blue-600 hover:underline">{contest.created_by}</span>
+                                    </Link>
+                                </td>
+                                <td>{formatDate(contest.startTime)}</td>
+                                <td>{formatDate(contest.endTime)}</td>
+                                <td>{contest.registerUser}</td>
+                                <td>
+                                    {contest.registered ? (
+                                        <Link href={`/contests/${contest.id}`}>
+                                            <button className="custom-button join-button">
+                                                <p className='font-normal'>Join</p>
+                                            </button>
+                                        </Link>
+                                    ) : (
+                                        <button 
+                                            className="custom-button register-button" 
+                                            onClick={() => handleRegister(contest.id)}
+                                        >
+                                            <p className='font-normal'>Register</p>
+                                        </button>
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p className='text-gray-300 font-bold disable-pointer-events'>No ongoing contest yet.</p>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    </section>
 
-            <section className="contest-section">
-                <div className="contest-container">
-                    <h2 className="contest-title">Upcoming Contest</h2>
-                    {upcomingContest ? (
-                        <table className="contest-table">
-                            <thead>
-                                <tr>
-                                    <th>Contest</th>
-                                    <th>Author</th>
-                                    <th>Start</th>
-                                    <th>End</th>
-                                    <th>Participants</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {upcomingContest.map((contest, cnt) => (
-                                    <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
-                                        <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
-                                        <td className="contest-author">
-                                            <Link href={`/loader/profile?id=${contest.created_by}`}>
-                                                <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
-                                            </Link>
-                                        </td>
-                                        <td>{formatDate(contest.startTime)}</td>
-                                        <td>{formatDate(contest.endTime)}</td>
-                                        <td>{contest.registerUser}</td>
-                                        <td>
-                                            {contest.registered ? (
-                                                <div className='text-gray-500'>Already registered</div>
-                                            ) : (
-                                                <button onClick={() => handleRegister(contest.id)} className="text-blue-600 hover:underline cursor-pointer">Register</button>
-                                            )}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-            </section>
+    <section className="contest-section">
+        <div className="contest-container">
+            <h2 className="contest-title">Upcoming Contest</h2>
+            {upcomingContest ? upcomingContest.length ? (
+                <table className="contest-table">
+                    <thead>
+                        <tr>
+                            <th>Contest</th>
+                            <th>Author</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th>Participants</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {upcomingContest.map((contest, cnt) => (
+                            <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
+                                <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
+                                <td className="contest-author">
+                                    <Link href={`/loader/profile?id=${contest.created_by}`}>
+                                        <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
+                                    </Link>
+                                </td>
+                                <td>{formatDate(contest.startTime)}</td>
+                                <td>{formatDate(contest.endTime)}</td>
+                                <td>{contest.registerUser}</td>
+                                <td>
+                                    {contest.registered ? (
+                                        <div className='text-gray-500 font-bold'>Already registered</div>
+                                    ) : (
+                                        <button onClick={() => handleRegister(contest.id)} className="custom-button register-button">Register</button>
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p className='text-gray-300 font-bold'>No upcoming contest yet.</p>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    </section>
 
-            <section className="contest-section">
-                <div className="contest-container">
-                    <h2 className="contest-title">Past Contest</h2>
-                    {pastContest ? (
-                        <table className="contest-table">
-                            <thead>
-                                <tr>
-                                    <th>Contest</th>
-                                    <th>Author</th>
-                                    <th>Start</th>
-                                    <th>End</th>
-                                    <th>Participants</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {pastContest.map((contest, cnt) => (
-                                    <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
-                                        <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
-                                        <td className="contest-author">
-                                            <Link href={`/loader/profile?id=${contest.created_by}`}>
-                                                <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
-                                            </Link>
-                                        </td>
-                                        <td>{formatDate(contest.startTime)}</td>
-                                        <td>{formatDate(contest.endTime)}</td>
-                                        <td>{contest.registerUser}</td>
-                                        <td>
-                                            <Link href={`/contests/${contest.id}`}>
-                                                <button className="text-blue-600 hover:underline cursor-pointer">Join</button>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-            </section>
+    <section className="contest-section">
+        <div className="contest-container">
+            <h2 className="contest-title">Past Contest</h2>
+            {pastContest ? (
+                <table className="contest-table">
+                    <thead>
+                        <tr>
+                            <th>Contest</th>
+                            <th>Author</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th>Participants</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {pastContest.map((contest, cnt) => (
+                            <tr key={contest.id} className={cnt % 2 === 0 ? "even-row" : "odd-row"}>
+                                <td className="contest-name">{contest.problemName}<br />({contest.access})</td>
+                                <td className="contest-author">
+                                    <Link href={`/loader/profile?id=${contest.created_by}`}>
+                                        <span className="text-blue-600 hover:underline cursor-pointer">{contest.created_by}</span>
+                                    </Link>
+                                </td>
+                                <td>{formatDate(contest.startTime)}</td>
+                                <td>{formatDate(contest.endTime)}</td>
+                                <td>{contest.registerUser}</td>
+                                <td>
+                                    <Link href={`/contests/${contest.id}`}>
+                                        <button className='custom-button join-button'>Join</button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    </section>
 
-            <Footer />
-        </>
+    <Footer />
+</>
+
     );
 };
 
