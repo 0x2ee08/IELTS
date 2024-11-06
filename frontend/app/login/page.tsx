2cca2156ca0d78551c9762e2c5eac1545609dbee
Spectrum3563 <tbq2107@gmail.com>
2024-11-06 20:55:52 +0700

    fix

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,11 +4,18 @@ import Header from '../components/Header';
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import config from '../config';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
 const LoginPage: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     const [status, setStatus] = useState('');
     const router = useRouter();
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
     const login = async () => {
         try {
@@ -56,12 +63,21 @@ const LoginPage: React.FC = () => {
                 </div>
                 <div className="mb-4">
                     <label className="block text-sm font-medium text-gray-700 mb-2">Enter Your Password:</label>
-                    <input
-                        type="password"
-                        className="border border-gray-300 px-3 py-2 rounded-md w-full"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                    <div className="relative">
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            className="border border-gray-300 px-3 py-2 rounded-md w-full"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button
+                            type="button"
+                            onClick={togglePasswordVisibility}
+                            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+                        >
+                            {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </button>
+                    </div>
                 </div>
                 <div className="mb-4 text-right">
                     <a href="/password_reset" className="text-sm text-blue-500 hover:underline">
